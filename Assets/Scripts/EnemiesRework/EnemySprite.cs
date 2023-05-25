6a3e29508d8eb9322707b67572ac9851ba1f7c7b
arasmek <125920478+arasmek@users.noreply.github.com>
2023-05-25 17:27:15 +0300

    New forest bg + enemy sprite flips

--- Assets/Scripts/EnemiesRework/EnemySprite.cs
+++ Assets/Scripts/EnemiesRework/EnemySprite.cs
@@ -7,12 +7,15 @@ public class EnemySprite : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     public Sprite enemy;
     public Sprite damage;
-
+    private Rigidbody2D rb2D;
     public bool takingDamage = false;
+    EnemyMovement movement;
 
     void Start()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        rb2D = GetComponent<Rigidbody2D>();
+        movement = GetComponent<EnemyMovement>();
     }
 
     void Update()
@@ -20,6 +23,18 @@ public class EnemySprite : MonoBehaviour
         if (takingDamage == true)
             spriteRenderer.sprite = damage;
         else spriteRenderer.sprite = enemy;
+
+        //rb2D.velocity = new Vector2(-movement.speed, rb2D.velocity.y);
+
+        // Check if the enemy is moving left
+        if (rb2D.velocity.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (rb2D.velocity.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
     }
     public IEnumerator DamageSprite()
     {
